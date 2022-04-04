69ccaf0b1a1baa612ef700ded18e50e36d66223a
firmanfa <fafirmansyah20@gmail.com>
2022-04-05 00:43:54 +0700

    adding dependencies

---
+++ app/src/main/java/com/binar/challenge4/database/UserDao.kt
@@ -0,0 +1,16 @@
+package com.binar.challenge4.database
+
+//import androidx.room.*
+//import com.example.myapplication.data.Student
+//import com.example.myapplication.data.User
+//
+//@Dao
+//interface UserDao {
+//
+//    @Query("SELECT * FROM User WHERE id = :id")
+//    fun login(id: Int):User
+//
+//    @Insert(onConflict = OnConflictStrategy.REPLACE)
+//    fun insertUser(user: User):Long
+//
+//}
